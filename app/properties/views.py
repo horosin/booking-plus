0608319447b0a4214f734ac6dd44e93f1ce96af1
Karol Horosin <karol.horosin@gmail.com>
2019-05-30 21:36:28 +0100

    verify booking periods

--- app/properties/views.py
+++ app/properties/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views import View, generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -13,6 +13,23 @@ from .forms import SearchPropertyForm
 
 import logging
 logger = logging.getLogger(__name__)
+
+
+def get_bookings_for_property(prop, date_from, date_to):
+    return prop.booking_set.filter(
+        Q(
+            date_from__gte=date_from,
+            date_from__lte=date_to
+        ) |
+        Q(
+            date_to__gte=date_from,
+            date_to__lte=date_to
+        ) |
+        Q(
+            date_from__lte=date_from,
+            date_to__gte=date_to
+        )
+    )
 
 
 class PropertyCreateView(generic.CreateView):
@@ -51,28 +68,16 @@ class PropertyListView(generic.ListView):
             city__icontains=data['city'],
             capacity__gte=data['people']
         )
-        
+
         # Filter booking for following conditions
         # booking starts anywhere between us
         # booking ends anywhere between us
         # booking starts before us and ends after us
         available_ids = []
         for prop in queryset:
-            bookings = prop.booking_set.filter(
-                Q(
-                    date_from__gte=data['date_from'],
-                    date_from__lte=data['date_to']
-                ) |
-                Q(
-                    date_to__gte=data['date_from'],
-                    date_to__lte=data['date_to']
-                ) |
-                Q(
-                    date_from__lte=data['date_from'],
-                    date_to__gte=data['date_to']
-                )
-            ).count()
-            if bookings == 0:
+            bookings_nb = get_bookings_for_property(
+                prop, data['date_from'], data['date_to']).count()
+            if bookings_nb == 0:
                 available_ids.append(prop.id)
 
         return queryset.filter(id__in=available_ids)
@@ -93,19 +98,22 @@ class AboutView(generic.TemplateView):
 class BookView(View):
 
     def get(self, request, *args, **kwargs):
-        property = get_object_or_404(Property, pk=self.kwargs['pk'])
+        prop = get_object_or_404(Property, pk=self.kwargs['pk'])
 
         context = {
-            'object': property,
+            'object': prop,
             'date_from': self.kwargs['date_from'],
             'date_to': self.kwargs['date_to']
         }
 
+        if get_bookings_for_property(prop, context['date_from'], context['date_to']).count() != 0:
+            raise Http404("Property is not available at this time!")
+        
         if request.GET.get('confirm'):
             Booking.objects.create(
-                property=property,
+                property=prop,
                 user=request.user,
-                # datetime.datetime.strptime(self.kwargs['date_from'], '%Y-%m-%d) 
+                # datetime.datetime.strptime(self.kwargs['date_from'], '%Y-%m-%d)
                 date_from=self.kwargs['date_from'],
                 date_to=self.kwargs['date_to']
             )
